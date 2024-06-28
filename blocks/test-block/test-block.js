16bd194e62201e4e35267c7515f1949c9f210f30
nanako <sakana.mossmoss0128@gmail.com>
2024-06-29 01:09:32 +0900

    test push
    test push

---
+++ blocks/test-block/test-block.js
@@ -0,0 +1,6 @@
+export default function decorate(block) {
+    // const test = document.getElementsByClassName("test-original-wrapper");
+    // const img = document.querySelector("img");
+    // test.removeChild(img);
+    console.log("test");
+}
